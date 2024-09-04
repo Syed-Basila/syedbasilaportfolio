8278761a59bdb8f7d9a40a3b8799d18e71ff758f
Syed Basila <123718024+Syed-Basila@users.noreply.github.com>
2024-09-05 00:42:37 +0530

    Add files via upload

---
+++ syedbasila-portfolio/js/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing", {
+    strings: ["Full Stack Web Developer", "Python Developer", "Digital Marketer", "Robotics& AI/ML"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+
+})
